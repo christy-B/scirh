60d8d850666cb1b4ad7a1607c7a8be880c6896a2
Ebou christelle <bienechristy94@gmail.com>
2025-12-18 16:18:26 +0100

    add api platform config and jwt config

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -6,9 +6,29 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[ApiResource(
+    shortName: 'utilisateurs',
+    description: 'les utilisateurs',
+    operations:[
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+        new Delete()
+    ]
+)]
+
 class User
 {
     #[ORM\Id]
